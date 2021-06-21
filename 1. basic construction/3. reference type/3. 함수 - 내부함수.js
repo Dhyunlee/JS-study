9ca480e3a566256b18b99867ba558953b887c883
leedong2897 <skyblue5030@naver.com>
2021-06-22 02:14:14 +0900

    함수부분 정리

--- 1. basic construction/3. reference type/3. 함수 - 내부함수.js
+++ 1. basic construction/3. reference type/3. 함수 - 내부함수.js
@@ -1,15 +1,11 @@
- /* 
-     내부 함수
-      : 함수 내부에 선언하는 함수
-  */
- function output() {
+// 내부 함수
+function output() {
     function innerfun() { //  innerfun()함수를 '내부 함수'라고 한다. 
         console.log("오늘도 힘내요!")
     }
     innerfun()
 }
- 
-   output()
-/* 
-   innerfun() (X)   
-    -> output()함수 내부에 선언된 innerfun()함수는 외부에서 선언할 수 없다. */
+
+output()
+
+// innerfun() (X)   
