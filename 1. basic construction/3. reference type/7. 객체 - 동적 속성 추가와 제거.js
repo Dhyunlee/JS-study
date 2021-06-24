f08fcac023b78282ea8b8e84e9f44866d4aded66
leedong2897 <skyblue5030@naver.com>
2021-06-24 16:59:15 +0900

    객체 속성추가, 객체요소 판별

--- 1. basic construction/3. reference type/7. 객체 - 동적 속성 추가와 제거.js
+++ 1. basic construction/3. reference type/7. 객체 - 동적 속성 추가와 제거.js
@@ -1,62 +1,42 @@
-window.onload = function () {
 
-/*
-    동적 속성 추가와 제거
-      : 처음 객체를 생성하는 시점 이후에 속성을 추가하거나 
-        제거하는 것을 "동적으로 속성을 추가/제거한다"라고 표현한다. 
-*/
+// 1. 동적으로 속성 추가
 
-/*  1. 동적으로 속성 추가
-        : 객체에 속성을 추가할 때에는 [객체.속성 = 추가할 내용]의 형태로 사용한다.  
-*/
+// 빈 객체 생성
+let student = {};
+console.log('[속성 없는 상태]: ' + student)
 
-    // 빈 객체 생성
-    let student = {};
-    console.log('[속성 없는 상태]: ' + student)
+// 속성 추가
+student.name = 'Mike';
+student.age = 29;
+student.gender = '남';
 
-    // 속성 추가
-    student.name = '이동현',
-        student.age = 29,
-        student.gender = '남';
-
-    // 메서드 추가   
-    student.toString = function () {
-        let output = '';
-        for (let key in this) {
-            if (key != 'toString') {
-                output += key + ': ' + this[key] + '\n';
-            }
+// 메서드 추가   
+student.toString = function () {
+    let output = '';
+    for (let key in this) {
+        if (key != 'toString') {
+            output += key + ': ' + this[key] + '\n';
         }
-        return output;
-    };
-    console.log("[속성 추가] \n" + student.toString());
-    //--------------------------------------------------------------------
+    }
+    return output;
+};
+console.log("[속성 추가] \n" + student.toString());
 
-/* 2. 동적으로 속성 수정(갱신)  
-    : 이미 존재하는 속성이라면 값을 수정합니다. 
-     -> 해당 속성에 값이 없으면 새로 값을 추가합니다. 
-*/
 
-    // 속성 수정(갱신)
-    student.name = "이동훈" // 속성 값을 이동훈으로 수정
-    student.age = 26,
+// 2. 동적으로 속성 수정(갱신)  
+
+// 속성 수정(갱신)
+student.name = "James" // 속성 값을 James으로 수정
+student.age = 26,
     console.log('[속성 수정]: ' + student.name + ' ' + student.age)
-    console.log("[수정된 속성 확인] \n" + student.toString())
-    console.log(" -> 이동현을 " + student.name + "으로 수정")
+console.log("[수정된 속성 확인] \n" + student.toString())
+console.log(" -> Mike을 " + student.name + "으로 수정")
 
 
-/*    
-    3. 동적으로 속성 제거
-       : 객체에 속성을 제거하려면 delete키워드를 사용한다. 
-        => 객체의 속성을 입력할 때는 typeof() 키워드 처럼 괄호를
-           사용해도 되고 사용하지 않아도 된다
-    
-*/
 
-    // 속성 제거
-    delete student.name  
-    // delete (student.name) : student객체의 name속성 제거 
-    console.log('[속성 제거]: ' + "속성 'name'이 제거됨 \n" + student); 
-    // toString()메서드를 사용하지 않아도 toString()메서드를 사용한다. 
+// 3. 동적으로 속성 제거
 
-}
+// 속성 제거
+delete student.name
+// delete (student.name) : student객체의 name속성 제거 
+console.log('[속성 제거]: ' + "속성 'name'이 제거됨 \n" + student);
--- 1. basic construction/3. reference type/8. 객체 - 객체요소 판별.js
+++ 1. basic construction/3. reference type/8. 객체 - 객체요소 판별.js
@@ -1,59 +1,45 @@
-window.onload = function () {
-
-    // 객체와 관련된 키워드 ('in' 과 'with')
-
-/*
-    1. in키워드
-      : <'key'> in <객체>형태로 쓴다. 
-       -> for in반복문과는 별도로 [in키워드]를 사용하면 
-          객체내에 해당 키가 있는 지 확인하는 키워드이다. 
-    */
-
-    //객체 생성
-    let student1 = {
-        이름: '이동현',
-        국어: 92, 수학: 80,
-        영어: 95, 과학: 98
-    };
-
-    console.log('이름' in student1); // true
-    console.log('성별' in student1); // false
-
-    //===========================================================
-
-/*
-    2. with키워드: with(객체) { <코드> }로 쓴다.
-     -> 복잡하게 사용해야 하는 코드를 짧게 줄어주는 키워드이다. 
- */
-
-    //객체 생성
-    let student2 = {
-        name: '이동현',
-        kor: 98, math: 77, eng: 86, sc: 86
-    }
-
-    // 예제
-    //      let output = '';
-    //      output += 'name' +  student2.name + '\n';
-    //      output += 'kor' +  student2.kor + '\n';
-    //      output += 'math' +  student2.math + '\n';
-    //      output += 'eng' +  student2.eng + '\n';
-    //      output += 'sc' +  student2.sc + '\n';
-    //      output += '총점' + (student2.kor +student2.math + student2.eng + student2.sc);
-    //      console.log(output);
+// in키워드
 
 
-    //위 예제를 with키워드로 짧게 줄어서 쓰자!
-    let output = '';
-    with (student2) {
-        output += 'name: ' + name + '\n';
-        output += 'kor: ' + kor + '\n';
-        output += 'math: ' + math + '\n';
-        output += 'eng: ' + eng + '\n';
-        output += 'sc: ' + sc + '\n';
-        output += 'total score: ' + (kor + math + eng + sc);
-    }
-    console.log(output);
-    //  -> ()안에 객체를 넣어주면  객체를 명시할 필요없이 속성을 쉽게 사용할 수 있다. 
+//객체 생성
+let student1 = {
+    이름: 'Mike',
+    국어: 92, 수학: 80,
+    영어: 95, 과학: 98
+};
+
+console.log('이름' in student1); // true
+console.log('성별' in student1); // false
+
+
+// 2. with키워드
+
+//객체 생성
+let student2 = {
+    name: '이동현',
+    kor: 98, math: 77, eng: 86, sc: 86
 }
+
+// 예제
+//      let output = '';
+//      output += 'name' +  student2.name + '\n';
+//      output += 'kor' +  student2.kor + '\n';
+//      output += 'math' +  student2.math + '\n';
+//      output += 'eng' +  student2.eng + '\n';
+//      output += 'sc' +  student2.sc + '\n';
+//      output += '총점' + (student2.kor +student2.math + student2.eng + student2.sc);
+//      console.log(output);
+
+
+//위 예제를 with키워드로 짧게 줄어서 쓰자!
+let output = '';
+with (student2) {
+    output += 'name: ' + name + '\n';
+    output += 'kor: ' + kor + '\n';
+    output += 'math: ' + math + '\n';
+    output += 'eng: ' + eng + '\n';
+    output += 'sc: ' + sc + '\n';
+    output += 'total score: ' + (kor + math + eng + sc);
+}
+console.log(output);
 
